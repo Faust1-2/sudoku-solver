218320d04e5a13e1c8453193db62dc193738f03c
faustin <faustin.dewas@efrei.net>
2023-06-27 11:31:59 +0200

    created readColumn function and test

--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -7,7 +7,15 @@ class Sudoku(var data: List[Integer]) {
 
     def readLine(line: Integer): List[Integer] = ???
 
-    def readColumn(column: Integer): List[Integer] = ???
+    def readColumn(column: Integer): List[Integer] = {
+        var columnList: List[Integer] = List()
+        for {
+            x <- 0 to 80
+            if (x % 9 == column)
+            y = data(x)
+        } yield columnList = columnList :+ y
+        return columnList
+    }
 
     def readSquare(square: Integer): List[Integer] = ???
 }
