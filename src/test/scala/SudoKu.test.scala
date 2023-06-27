e7abdbb132f2669d6c473b74ac465c12421c8c15
faustin <faustin.dewas@efrei.net>
2023-06-27 15:33:38 +0200

    refactored tests

--- src/test/scala/SudoKu.test.scala
+++ src/test/scala/SudoKu.test.scala
@@ -1,11 +1,8 @@
 import sudoku.Sudoku
 
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
-  }
+
+  val sudoKu = Sudoku(List.range(0, 81).map(x => x%9))
 
   test("isValid") {}
 
@@ -15,34 +12,23 @@ class MySuite extends munit.FunSuite {
 
   test("readColumn") {
     val wantedColumn = 3
-    var myList: List[Int] = List()
     var testList: List[Int] = List()
     for (x <- 0 to 80) {
-      myList = myList :+ x
       if (x % 9 == wantedColumn) {
-        testList = testList :+ x
+        testList = testList :+ x%9
       }
     }
-    val mySudoKu = Sudoku(myList)
-
-    assertEquals(mySudoKu.readColumn(wantedColumn), testList)
+    assertEquals(sudoKu.readColumn(wantedColumn), testList)
   }
 
   test("readSquare") {
-    val sudoku = Sudoku(List.range(0, 81))
-    val result = List(30, 31, 32, 39, 40, 41, 48, 49, 50)
-
-    assertEquals(sudoku.readSquare(5), result)
+    val result = List(3, 4, 5, 3, 4, 5, 3, 4, 5)
+    assertEquals(sudoKu.readSquare(5), result)
   }
 
-  test("PrettyPrint") {
-    var myList: List[Int] = List()
-    for (x <- 0 to 80) {
-      myList = myList :+ x%9
-    }
-    val mySudoKu = Sudoku(myList)
+  test("toString") {
     assertNoDiff(
-      mySudoKu.toString(),
+      sudoKu.toString(),
       """ ___ ___ ___ ___ ___ ___ ___ ___ ___
 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
