e43e0e64a7b496fccb9718025be328300f29af0d
faustin <faustin.dewas@efrei.net>
2023-06-27 16:45:46 +0200

    updated tests

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -11,7 +11,7 @@ class MySuite extends munit.FunSuite {
   test("isSolved") {}
 
   test("isValid") {
-  val myList: List[Int] = List(
+  val invalidRow: List[Int] = List(
       5, 3, 0, 7, 7, 0, 0, 0, 0,
       6, 0, 0, 1, 9, 5, 0, 0, 0,
       0, 9, 8, 0, 0, 0, 0, 6, 0,
@@ -22,9 +22,49 @@ class MySuite extends munit.FunSuite {
       0, 0, 0, 4, 1, 9, 0, 0, 5,
       0, 0, 0, 0, 8, 0, 0, 7, 0
     )
-  val mySudoku = new Sudoku(myList)
+  val invalidColumn: List[Int] = List(
+      5, 3, 0, 2, 7, 0, 0, 0, 0,
+      6, 0, 0, 1, 9, 5, 0, 0, 0,
+      5, 9, 8, 0, 0, 0, 0, 6, 0,
+      8, 0, 0, 0, 6, 0, 0, 0, 3,
+      4, 0, 0, 8, 0, 3, 0, 0, 1,
+      7, 0, 0, 0, 2, 0, 0, 0, 6,
+      0, 6, 0, 0, 0, 0, 2, 8, 0,
+      0, 0, 0, 4, 1, 9, 0, 0, 5,
+      0, 0, 0, 0, 8, 0, 0, 7, 0
+    )
+  val invalidSquare: List[Int] = List(
+      5, 3, 0, 2, 7, 0, 0, 0, 0,
+      6, 0, 0, 1, 9, 5, 0, 0, 0,
+      0, 9, 5, 0, 0, 0, 0, 6, 0,
+      8, 0, 0, 0, 6, 0, 0, 0, 3,
+      4, 0, 0, 8, 0, 3, 0, 0, 1,
+      7, 0, 0, 0, 2, 0, 0, 0, 6,
+      0, 6, 0, 0, 0, 0, 2, 8, 0,
+      0, 0, 0, 4, 1, 9, 0, 0, 5,
+      0, 0, 0, 0, 8, 0, 0, 7, 0
+    )
+  val valid: List[Int] = List(
+      5, 3, 0, 2, 7, 0, 0, 0, 0,      
+      6, 0, 0, 1, 9, 5, 0, 0, 0,
+      0, 9, 2, 0, 0, 0, 0, 6, 0,
+      8, 0, 0, 0, 6, 0, 0, 0, 3,
+      4, 0, 0, 8, 0, 3, 0, 0, 1,
+      7, 0, 0, 0, 2, 0, 0, 0, 6,
+      0, 6, 0, 0, 0, 0, 2, 8, 0,
+      0, 0, 0, 4, 1, 9, 0, 0, 5,
+      0, 0, 0, 0, 8, 0, 0, 7, 0
+    )
 
-  assert(!mySudoku.isValid())
+  val rowSudoku = new Sudoku(invalidRow)
+  val columnSudoku = new Sudoku(invalidColumn)
+  val squareSudoku = new Sudoku(invalidSquare)
+  val validSudoku = new Sudoku(valid)
+
+  assert(!rowSudoku.isValid())
+  assert(!columnSudoku.isValid())
+  assert(!squareSudoku.isValid())
+  assert(validSudoku.isValid())
 }
 
 
