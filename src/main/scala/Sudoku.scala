867be7912048d997690f1efef0f2433380b64157
Turdyo <martin.gayet@efrei.net>
2023-06-27 10:59:13 +0200

    feat(sudoku): add helpers functions definition

---
+++ src/main/scala/Sudoku.scala
@@ -0,0 +1,13 @@
+package sudoku
+
+class Sudoku(var data: List[Integer]) {
+    def isSolved(): Boolean = ???
+
+    def isValid(): Boolean = ??? // contains error
+
+    def readLine(line: Integer): List[Integer] = ???
+
+    def readColumn(column: Integer): List[Integer] = ???
+
+    def readSquare(square: Integer): List[Integer] = ???
+}
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,30 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+import sudoku.Sudoku
+
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
     val obtained = 42
     val expected = 42
     assertEquals(obtained, expected)
   }
+
+  test("isValid") {
+
+  }
+
+  test("isSolved") {
+
+  }
+
+  test("readLine") {
+
+  }
+
+  test("readColumn") {
+
+  }
+
+  test("readSquare") {
+
+  }
+
 }
