d16dd219ffb6e2857ab5ad08997b23c8b0377fa4
faustin <faustin.dewas@efrei.net>
2023-06-29 19:53:45 +0200

    refactor tests

--- src/test/scala/SudoKu.test.scala
+++ src/test/scala/SudoKu.test.scala
@@ -19,49 +19,49 @@ class MySuite extends munit.FunSuite {
   test("isSolved") {}
 
   test("isValid") {
-  val invalidRow: List[Int] = List(
-      5, 3, 0, 7, 7, 0, 0, 0, 0,
-      6, 0, 0, 1, 9, 5, 0, 0, 0,
-      0, 9, 8, 0, 0, 0, 0, 6, 0,
-      8, 0, 0, 0, 6, 0, 0, 0, 3,
-      4, 0, 0, 8, 0, 3, 0, 0, 1,
-      7, 0, 0, 0, 2, 0, 0, 0, 6,
-      0, 6, 0, 0, 0, 0, 2, 8, 0,
-      0, 0, 0, 4, 1, 9, 0, 0, 5,
-      0, 0, 0, 0, 8, 0, 0, 7, 0
+  val invalidRow: Array[Array[Int]] = Array(
+      Array(5, 3, 0, 7, 7, 0, 0, 0, 0),
+      Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+      Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
+      Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+      Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+      Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+      Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+      Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+      Array(0, 0, 0, 0, 8, 0, 0, 7, 0)
     )
-  val invalidColumn: List[Int] = List(
-      5, 3, 0, 2, 7, 0, 0, 0, 0,
-      6, 0, 0, 1, 9, 5, 0, 0, 0,
-      5, 9, 8, 0, 0, 0, 0, 6, 0,
-      8, 0, 0, 0, 6, 0, 0, 0, 3,
-      4, 0, 0, 8, 0, 3, 0, 0, 1,
-      7, 0, 0, 0, 2, 0, 0, 0, 6,
-      0, 6, 0, 0, 0, 0, 2, 8, 0,
-      0, 0, 0, 4, 1, 9, 0, 0, 5,
-      0, 0, 0, 0, 8, 0, 0, 7, 0
+  val invalidColumn: Array[Array[Int]] = Array(
+      Array(5, 3, 0, 2, 7, 0, 0, 0, 0),
+      Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+      Array(5, 9, 8, 0, 0, 0, 0, 6, 0),
+      Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+      Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+      Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+      Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+      Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+      Array(0, 0, 0, 0, 8, 0, 0, 7, 0)
     )
-  val invalidSquare: List[Int] = List(
-      5, 3, 0, 2, 7, 0, 0, 0, 0,
-      6, 0, 0, 1, 9, 5, 0, 0, 0,
-      0, 9, 5, 0, 0, 0, 0, 6, 0,
-      8, 0, 0, 0, 6, 0, 0, 0, 3,
-      4, 0, 0, 8, 0, 3, 0, 0, 1,
-      7, 0, 0, 0, 2, 0, 0, 0, 6,
-      0, 6, 0, 0, 0, 0, 2, 8, 0,
-      0, 0, 0, 4, 1, 9, 0, 0, 5,
-      0, 0, 0, 0, 8, 0, 0, 7, 0
+  val invalidSquare: Array[Array[Int]] = Array(
+      Array(5, 3, 0, 2, 7, 0, 0, 0, 0),
+      Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+      Array(0, 9, 5, 0, 0, 0, 0, 6, 0),
+      Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+      Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+      Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+      Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+      Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+      Array(0, 0, 0, 0, 8, 0, 0, 7, 0)
     )
-  val valid: List[Int] = List(
-      5, 3, 0, 2, 7, 0, 0, 0, 0,      
-      6, 0, 0, 1, 9, 5, 0, 0, 0,
-      0, 9, 2, 0, 0, 0, 0, 6, 0,
-      8, 0, 0, 0, 6, 0, 0, 0, 3,
-      4, 0, 0, 8, 0, 3, 0, 0, 1,
-      7, 0, 0, 0, 2, 0, 0, 0, 6,
-      0, 6, 0, 0, 0, 0, 2, 8, 0,
-      0, 0, 0, 4, 1, 9, 0, 0, 5,
-      0, 0, 0, 0, 8, 0, 0, 7, 0
+  val valid: Array[Array[Int]] = Array(
+      Array(5, 3, 0, 2, 7, 0, 0, 0, 0),      
+      Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+      Array(0, 9, 2, 0, 0, 0, 0, 6, 0),
+      Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+      Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+      Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+      Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+      Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+      Array(0, 0, 0, 0, 8, 0, 0, 7, 0)
     )
 
   val rowSudoku = new Sudoku(invalidRow)
@@ -86,12 +86,36 @@ class MySuite extends munit.FunSuite {
         testList = testList :+ x%9
       }
     }
-    assertEquals(sudokuTable.readColumn(wantedColumn), testList)
+    assertEquals(sudokuTable.readColumn(wantedColumn).toSeq, testList.toSeq)
   }
 
+  /**
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * ----------------------
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * ----------------------
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 0 1 2 | 3 4 5 | 6 7 8
+  * 
+  * 0 | 1 | 2
+  * 3 | 4 | 5
+  * 6 | 7 | 8
+  */
   test("readSquare") {
-    val result = Array(3, 4, 5, 3, 4, 5, 3, 4, 5)
-    assertEquals(sudokuTable.readSquare(5).toSeq, result.toSeq)
+    assertEquals(sudokuTable.readSquare(0).toSeq, Seq(0, 1, 2, 0, 1, 2, 0, 1, 2))
+    assertEquals(sudokuTable.readSquare(1).toSeq, Seq(3, 4, 5, 3, 4, 5, 3, 4, 5))
+    assertEquals(sudokuTable.readSquare(2).toSeq, Seq(6, 7, 8, 6, 7, 8, 6, 7, 8))
+    assertEquals(sudokuTable.readSquare(3).toSeq, Seq(0, 1, 2, 0, 1, 2, 0, 1, 2))
+    assertEquals(sudokuTable.readSquare(4).toSeq, Seq(3, 4, 5, 3, 4, 5, 3, 4, 5))
+    assertEquals(sudokuTable.readSquare(5).toSeq, Seq(6, 7, 8, 6, 7, 8, 6, 7, 8))
+    assertEquals(sudokuTable.readSquare(6).toSeq, Seq(0, 1, 2, 0, 1, 2, 0, 1, 2))
+    assertEquals(sudokuTable.readSquare(7).toSeq, Seq(3, 4, 5, 3, 4, 5, 3, 4, 5))
+    assertEquals(sudokuTable.readSquare(8).toSeq, Seq(6, 7, 8, 6, 7, 8, 6, 7, 8))
   }
 
   test("toString") {
