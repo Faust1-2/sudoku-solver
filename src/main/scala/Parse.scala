4700513d79557e369fba887b84c0dc27a7db9cde
faustin <faustin.dewas@efrei.net>
2023-07-03 23:22:00 +0200

    added validateFormat

--- src/main/scala/Parse.scala
+++ src/main/scala/Parse.scala
@@ -5,16 +5,37 @@ import scala.io.Source
 import zio._
 import zio.json._
 import scala.util.boundary
+import java.io.InvalidObjectException
 
 type Grid = Array[Array[Int]]
 
 def parseFile(path: String): ZIO[Any, Throwable, Grid] = {
   val jsonString = Source.fromFile(path).mkString
   val maybeSudoku = jsonString.fromJson[Grid]
-
+  
   maybeSudoku.match
-    case Right(value) => ZIO.from(value)
+    case Right(value) => {
+      if (validateFormat(value)) ZIO.from(value)
+      else throw InvalidObjectException(value.toString())
+    }
     case Left(value) => throw FileNotFoundException()
 }
 
-def validateFormat(grid: Grid): Boolean = ???
+def validateFormat(grid: Grid): Boolean = {
+  val acceptedValues = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+  boundary:
+    if(grid.length == 9) {
+      for (line <- grid) {
+        if(line.length != 9) boundary.break(false)
+        for (value <- line) {
+          if(!acceptedValues.contains(value)) {
+            boundary.break(false)
+          }
+        }
+      }
+      boundary.break(true)
+    }
+    else {
+      boundary.break(false)
+    }
+}
